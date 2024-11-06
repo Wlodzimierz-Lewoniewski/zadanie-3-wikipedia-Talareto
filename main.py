05dd40c325350ee58a929bdb1033fcd988e8350e
Talareto <99801682+Talareto@users.noreply.github.com>
2024-11-06 22:36:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,35 +3,30 @@ from bs4 import BeautifulSoup
 import html
 
 def fetch_article_details(article_path):
-    base_url = 'https://pl.wikipedia.org'
-    url = f'{base_url}{article_path}'
+    url = f'https://pl.wikipedia.org{article_path}'
     response = requests.get(url)
-
     if response.status_code == 200:
-        page_content = response.text
-        soup = BeautifulSoup(page_content, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
         details = []
-
         body_content = soup.find("div", class_="mw-body-content")
-        internal_links = [
-            a.get('title') for a in body_content.find_all('a', href=True)
-            if a['href'].startswith('/wiki/') and ':' not in a['href'][6:]
-        ][:5]
+        internal_links = [a.get('title') for a in body_content.find_all('a', href=True)
+                          if a['href'].startswith('/wiki/') and ':' not in a['href'][6:]][:5]
         details.append(" | ".join(internal_links))
-
-        images = [img['src'] for img in body_content.find_all("img") if '/wiki/' not in img['src']][:3]
+        images = [img['src'] for img in body_content.find_all("img") if img['src'].startswith('//upload')][:3]
         details.append(" | ".join(images) if images else "")
+
 
         references_section = soup.find("div", class_="mw-references-wrap") or soup.find("div", class_="refsection")
         external_links = []
         if references_section:
             for ref in references_section.find_all("li"):
                 for link in ref.find_all("a", href=True):
-                    if "http" in link['href']:
+                    if link['href'].startswith("http"):
                         external_links.append(html.escape(link['href']))
                         if len(external_links) == 3:
                             break
         details.append(" | ".join(external_links))
+
 
         category_section = soup.find("div", class_="mw-normal-catlinks")
         if category_section:
@@ -42,7 +37,7 @@ def fetch_article_details(article_path):
 
         return details
     else:
-        print("Błąd pobierania strony:", response.status_code)
+        print("Błąd podczas pobierania strony:", response.status_code)
         return []
 
 def main():
@@ -61,8 +56,7 @@ def main():
             for detail in article_details:
                 print(detail)
     else:
-        print("Błąd pobierania strony:", response.status_code)
+        print("Błąd podczas pobierania strony:", response.status_code)
 
 if __name__ == "__main__":
     main()
-
